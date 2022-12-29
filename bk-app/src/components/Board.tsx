9aeecbeadae71f3c1fff28d09e9767b3bf9f681f
Brennan Caldwell <brennan.caldwell@gmail.com>
2022-12-29 13:39:11 -0500

    Add close button & tweak styles

--- bk-app/src/components/Board.tsx
+++ bk-app/src/components/Board.tsx
@@ -93,7 +93,8 @@ function Board({ close }: BoardProps) {
 
   return (
     <div className="bg-white overflow-scroll border border-nyt-grey rounded">
-        <h1 className="">Bee Keeper</h1>
+      <img src="data:image/svg+xml,%3C%3Fxml version='1.0' standalone='no'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 20010904//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'%3E%3Csvg version='1.0' xmlns='http://www.w3.org/2000/svg' width='512.000000pt' height='512.000000pt' viewBox='0 0 512.000000 512.000000' preserveAspectRatio='xMidYMid meet'%3E%3Cg transform='translate(0.000000,512.000000) scale(0.100000,-0.100000)'%0Afill='%23000000' stroke='none'%3E%3Cpath d='M161 5103 c-88 -31 -161 -136 -161 -233 0 -20 7 -59 16 -86 15 -45%0A106 -139 1103 -1137 l1086 -1087 -1082 -1083 c-594 -595 -1089 -1097 -1098%0A-1116 -45 -90 -24 -213 50 -286 69 -70 163 -91 261 -59 45 15 139 106 1136%0A1103 l1088 1086 1088 -1086 c997 -997 1091 -1088 1136 -1103 98 -32 192 -11%0A261 59 70 69 91 163 59 261 -15 45 -106 139 -1103 1137 l-1086 1087 1086 1088%0Ac997 997 1088 1091 1103 1136 32 98 11 192 -59 261 -69 70 -163 91 -261 59%0A-45 -15 -139 -106 -1137 -1103 l-1087 -1086 -1088 1086 c-997 997 -1091 1088%0A-1136 1103 -61 20 -117 19 -175 -1z'/%3E%3C/g%3E%3C/svg%3E%0A" alt="Close Bee Keeper" onClick={close} className="cursor-pointer absolute top-4 right-5 w-3"></img>
+        <h1 id="keeper-headline" className="font-bold nyt-franklin">Bee Keeper</h1>
         <select value={gridView} onChange={(e) => setGridView(e.target.value)} name="keeper-grid-view" id="keeper-grid-view">
           <option value="all">All</option>
           <option value="remaining">Remaining</option>
@@ -117,8 +118,11 @@ function Board({ close }: BoardProps) {
             }
           </table>
         </div>
+        <p className="text-xs"><a target="_blank" className="underline" href="https://icons8.com/icon/ocTQkWmUEZQW/beehive">Beehive</a> icon by <a target="_blank" className="underline" href="https://icons8.com">Icons8</a></p>
     </div>
   )
 }
+
+
 
 export default Board;
